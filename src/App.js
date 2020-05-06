7d9bbd856343eaec230d3eff56e47d12f2b893c6
Alek <56727628+aseawright1@users.noreply.github.com>
2020-05-05 20:29:06 -0500

    create navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter} from 'react-router-dom';
+import Routes from './components/routes';
+import { Link } from 'react-router-dom'
+// React-MDL components
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+// Styling
+import './assets/css/App.css';
+import homeBG from './assets/images/home-bg.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layout style={{ backgroundImage: 'url(' + homeBG + ')'}}>
+          <Header transparent title="C O S M O L O G U E" style={{ color: 'white' }}>
+            <Navigation>
+              <Link to="/">Home</Link>
+              <Link to="/events">Events</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/signup">Signup</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="COSMOLOGUE" style={{ backgroundColor: 'black', color: 'white' }}>
+            <Navigation>
+              <Link style={{ color: 'white' }} to="/">Home</Link>
+              <Link style={{ color: 'white' }} to="/events">Events</Link>
+              <Link style={{ color: 'white' }} to="/profile">Profile</Link>
+              <Link style={{ color: 'white' }} to="/signup">Signup</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content">
+              <Routes/>
+            </div>
+          </Content>
+        </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
