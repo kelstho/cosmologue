97fa53992bde5de5b52c1ac0153eacc55979d7b1
INWYTIA <inwytia@gmail.com>
2020-05-06 21:18:31 -0500

    updated package.json and adjusted server.js for build

--- server.js
+++ server.js
@@ -4,18 +4,13 @@ const routes = require("./routes");
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cosmologue");
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
 
 app.listen(port, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${port}!`);
