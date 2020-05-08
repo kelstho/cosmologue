1aedfbbdb1db2b7e02603cbddf41f560ba53d771
Kelly Thompson <ksthompson48@gmail.com>
2020-05-08 14:51:14 -0500

    add test code to Home page & Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -38,21 +38,34 @@ class Profile extends Component {
             <h1>Profile</h1>
           </div>
           <div className="row">
-            {/* <h4>Welcome {this.state.name}</h4> */}
+            <h4>Welcome User!</h4>
           </div>
           <div className="row">
             <div className="col s12">
-              {/* <Horoscope horoscope={this.state.horoscope} /> */}
+              <p>Horoscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blahHoroscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blah Horoscope horoscope horoscope blah blah blah</p>
             </div>
           </div>
         </div>
         <div className="container">
           <div className="row">
             <div className="col s12 m5">
-              {/* <SavedEvents events={this.state.events} /> */}
+              <ul className="collection">
+                <li className="collection-item">
+                  <a href="#">Lunary Event</a>
+                </li>
+                <li className="collection-item">
+                  <a href="#">Solar Event</a>
+                </li>
+                <li className="collection-item">
+                  <a href="#">Celestial Event</a>
+                </li>
+                <li className="collection-item">
+                  <a href="#">Party Event</a>
+                </li>
+              </ul>
             </div>
             <div className="col s12 m7">
-              {/* <ClickedEvent /> */}
+              <p>Details about clicked event Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. </p>
             </div>
           </div>
         </div>
@@ -60,5 +73,13 @@ class Profile extends Component {
     )
   }
 };
+
+{/* <h4>Welcome {this.state.name}</h4> */ }
+
+{/* <Horoscope horoscope={this.state.horoscope} /> */ }
+
+{/* <SavedEvents events={this.state.events} /> */ }
+
+{/* <ClickedEvent /> */ }
 
 export default Profile;
