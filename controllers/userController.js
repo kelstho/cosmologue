2560a62fee9250a64f76fcc0e02e1a590e8d9f1a
INWYTIA <inwytia@gmail.com>
2020-05-11 20:30:51 -0500

    passport configured

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
 const db = require("../models");
 
+// db.UserData.register({username:'paul', active: false}, 'paul');
+// db.UserData.register({username:'jay', active: false}, 'jay');
+// db.UserData.register({username:'roy', active: false, sign: 'libra'}, 'roy');
+
 module.exports = {
   create: function(req, res) {
-    db.User.create(req.body)
+    db.UserData.register({username: req.body.username, sign: req.body.username}, req.body.password)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+var session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const path = require('path');
+var passport = require("./config/passport");
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cosmologue");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cosmologue",
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${port}!`);
