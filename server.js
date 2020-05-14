276504da1c5fc9b6fa42b612e55ead6eb1226a93
Alek <56727628+aseawright1@users.noreply.github.com>
2020-05-13 19:47:58 -0500

    update files for heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cosmologue",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:cosmologue1@ds129469.mlab.com:29469/heroku_f4r7d3rj",
 { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, function() {
