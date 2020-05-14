611907812891a38170b24aabaca81567bcf3cc84
Alek <56727628+aseawright1@users.noreply.github.com>
2020-05-13 20:59:28 -0500

    testing favicon

--- client/src/components/ClickedEvent.js
+++ client/src/components/ClickedEvent.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function ClickedEvent(props) {
   return <p id="clickedEvent">{props.events.event}</p>
+  // document.getElementById()
 }
 
 export default ClickedEvent;
