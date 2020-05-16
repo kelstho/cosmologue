c1bc572b732153cf4e320adf9abaea5c86a9302a
INWYTIA <inwytia@gmail.com>
2020-05-16 09:52:09 -0500

    login successful, user data routes made

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
+const passport = require("../../config/passport");
 
 // Matches with "/api/user"
 router
   .route("/")
   .post(userController.create);
 
-module.exports = router;
+  // Matches with "/api/user/login"
+router
+  .post("/login", passport.authenticate('local'), function (req, res) {
+  console.log(req);
+  console.log(res);
+  res.json({username: req.user.username,
+    redirectURI: "/"});
+});
+
+  // Matches with "api/user/data"
+router
+  .route("/data")
+  .get(userController.data)
+  .post(userController.update);
+
+module.exports = router;
