cbffb4aed1fc04de50744184ab1a673c306c5d31
INWYTIA <inwytia@gmail.com>
2020-05-20 20:31:53 -0500

    backend calendar api now updating database with all events of current year.

--- API/calendar.js
+++ API/calendar.js
@@ -1,14 +1,72 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
 const db = require("../models");
 
 module.exports = {
-  update: function(event) {
-    db.CalEvent.update({
-      year: event.year,
-      month: event.month,
-      day: event.day,
-      title: event.title
-    }, event, {upsert: true})
-    .then(dbModel => console.log("Updated " + dbModel.title))
-    .catch(err => console.log(err));
+  updateDB: async function() {
+    var d = new Date();
+    let currentYear = d.getFullYear().toString();
+    const months = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+    var arr = [];
+    // call the source
+    for (currentMonth of months) {
+      await axios.get(`https://in-the-sky.org/newscal.php?year=${currentYear}&month=${currentMonth}&maxdiff=7`)
+        .then(function (response) {  
+        // Load the Response into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(response.data);
+    
+        // With cheerio, find each newcalitem div that contains an 'a' tag
+        $("div.newscalitem > a").each(function (i, element) {
+    
+          // Save the text of the element in variables
+    
+          var link = $(element).attr("href");
+    
+          var year = link.slice(35, -11);
+          var month = link.slice(39, -9);
+          var day = link.slice(41, -7);
+    
+          var dateYear = year;
+          var dateMonth = month;
+          var dateDay = day;
+          var titleStr = $(element).text();
+          var title = titleStr;
+          var summaryStr = $(element).attr("title");
+          var summary = summaryStr;
+    
+          const eventsData =
+            {
+              year: dateYear,
+              month: dateMonth,
+              day: dateDay,
+              title: title,
+              link: dateYear,
+              summary: summary,
+            };
+    
+          arr.push(eventsData)
+    
+        })    
+      })
+    }
+    arr.forEach((event) => {
+      db.CalEvent.updateOne({
+        year: event.year,
+        month: event.month,
+        day: event.day,
+        title: event.title
+      },
+      event,
+      {upsert: true},
+      (err, writeOpResult) => {
+        if (err) {
+          console.log(err)
+        }else {
+          console.log("Successful update!");
+        }
+      })
+    });
   }
 };
