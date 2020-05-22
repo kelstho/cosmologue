7540f0cd5328811b53ed7d4a0f8e885f0d1d567d
Kelly Thompson <ksthompson48@gmail.com>
2020-05-22 13:41:22 -0500

    news API & daily pic API functioning

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,13 @@ import axios from "axios";
 export default {
   getUserInfo: function () {
     return axios.get("/api/user/data")
+  },
+
+  getAllNews: function () {
+    return axios.get("/api/article")
+  },
+
+  getDailyPic: function () {
+    return axios.get("/api/picOTD")
   }
 }
